fe7597c5b19c8ffeb5a7f49a03fbdc9ac6213b25
Vladimr Kondratsenko <vovanych1981@gmail.com>
2023-04-13 18:10:23 +0300

    add

--- BusOProject/bus.h
+++ BusOProject/bus.h
@@ -1,28 +1,23 @@
 #include "main.h"
 
-
-class bus {
+class Bus {
 public:
 	string brand;
 	string number;
 	int price;
 	int capacity;
+
+	Bus() {
+		brand = "no brand";
+		number = "XXXXPM7";
+		price = 0;
+		capacity = 0;
+	}
+
+	Bus(string brnd, string num, int prc, int cpct) {
+		brand = brnd;
+		number = num;
+		price = prc;
+		capacity = cpct;
+	}
 };
-
-bus() {
-	brand = "nit specified";
-	number = "XXXXPM7";
-	price = 0;
-	capacity = 0;
-
-}
-bus(string brnd, string num, int prc, int cpct) {
-	brand = brnd;
-	number = num;
-	price = prc;
-	capacity = cpct
-
-
-
-}
-
