8d527bcd6e7d1110ae5f5f88e59bba3f0b315864
Vladimr Kondratsenko <vovanych1981@gmail.com>
2023-04-13 17:45:28 +0300

    Add project files.

---
+++ BusOProject/bus.h
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+class bus {
+	public;
+	string brand;
+	string number;
+	int price;
+	int capacity
+};
+
+
---
+++ BusOProject/main.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "bus.h"
+
+int main() {
+
+	bus bus1;
+
+	return 0;
+}
+
+
+
