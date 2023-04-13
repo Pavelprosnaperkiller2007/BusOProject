22ff822138fcb0416bab9eacc7c060d78d6b1aee
Vladimr Kondratsenko <vovanych1981@gmail.com>
2023-04-13 18:08:32 +0300

    add

--- BusOProject/bus.h
+++ BusOProject/bus.h
@@ -2,11 +2,27 @@
 
 
 class bus {
-	public;
+public:
 	string brand;
 	string number;
 	int price;
-	int capacity
+	int capacity;
 };
 
+bus() {
+	brand = "nit specified";
+	number = "XXXXPM7";
+	price = 0;
+	capacity = 0;
+
+}
+bus(string brnd, string num, int prc, int cpct) {
+	brand = brnd;
+	number = num;
+	price = prc;
+	capacity = cpct
+
+
+
+}
 
--- BusOProject/main.cpp
+++ BusOProject/main.cpp
@@ -1,9 +1,23 @@
+
+// Bus 1: MAZ, 1234PM7, $ 14000, 10	-
+// Bus 2: PAZ, 2345PM7, $ 23500, 15	- 
+// Bus 3: Mercedes Benz, 3456PM7, $ 21650, 17	- 
+// Bus 4: Scania, 4567PM7, $ 45000, 30	- 
+// Bus 5: Neoplan, 5678PM7, $ 113000, 87	- 
+
+
 #include "main.h"
 #include "bus.h"
 
 int main() {
+	Bus bus1("MAZ", "1234PM7", 14000, 10);
+	Bus bus2("PAZ", "2345PM7", 23500, 15);
+	Bus bus3("Mercedes Benz", "3456PM7", 21560, 17);
+	Bus bus4("Scenia", "4567PM7", 45000, 30);
+	Bus bus5("Neoplan", "5678PM7", 113000, 87);
 
-	bus bus1;
+
+
 
 	return 0;
 }
